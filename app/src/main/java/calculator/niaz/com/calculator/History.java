8187437665b39d0d322e59b451dbab22083c7a70
Niaz <trialnly@gmail.com>
2018-03-12 22:41:37 +0600

    Mind SDK set to API 8 and uncessary dependecies removed

--- app/src/main/java/calculator/niaz/com/calculator/History.java
+++ app/src/main/java/calculator/niaz/com/calculator/History.java
@@ -1,13 +1,11 @@
 package calculator.niaz.com.calculator;
 
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-public class History extends AppCompatActivity {
+public class History extends Activity
+{
     TextView ht ;
 
     @Override
--- app/src/main/java/calculator/niaz/com/calculator/Main.java
+++ app/src/main/java/calculator/niaz/com/calculator/Main.java
@@ -1,19 +1,16 @@
 package calculator.niaz.com.calculator;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.mariuszgromada.math.mxparser.Expression;
 
-
-import org.mariuszgromada.math.mxparser.*;
-
-public class Main extends AppCompatActivity implements View.OnClickListener{
+public class Main extends Activity implements View.OnClickListener{
       Button btn[] = new Button[20];
       TextView textBox;
       Expression e1;
@@ -56,7 +53,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener{
 
             }
             result = getExpressionResultMx(previousText);
-           
+
             if(!result.equals("NaN"))
             {
                 String hist = previousText + " = " + result + "\n\n";
